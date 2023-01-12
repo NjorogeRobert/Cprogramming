41dfdf965c161d4cea29e5fb02cebb89cdc6c764
NjorogeMaina <mainarobertn@gmail.com>
2023-01-12 15:14:57 +0300

    singly_linked lists with stacks

--- Stacks/README.md
+++ Stacks/README.md
@@ -1,5 +1,6 @@
 In this project we get to understand the Array Implementation in Stacks.
-
+TASKS/PROJECTS
+I. stack_array.c
 FUNCTIONS USED
 Push(): function that is responsible of adding items to the stack
 Pop(): function that is responsible of deleting Items from the stack
@@ -20,3 +21,22 @@ Variables Used
 stack_arr[MAX]: represents the stack
 #define MAX: preprocessor containing the maximum value of array stack_arr[]
 int top: index of the values in the array.
+
+II. sin_push_stack
+Shows how Singly_linked lists can be used with stacks.
+
+FUNCTIONS USED
+1. Global referential structure with a global pointer top
+struct node
+{
+	int data;
+	struct node *link;
+}*top = NULL;
+
+2. push(): Functions responsible in putting items into stack
+
+3. seek(): Function responsible in printing the top most data 
+	   in the stack.
+
+4. print(): function responsible for printing all the data in the
+	    stack.
---
+++ Stacks/sin_push_stack.c
@@ -0,0 +1,89 @@
+//Initialize the header files needed
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+// initialize global structure node and global pointer top
+
+struct node
+{
+	int data;
+	struct node *link;
+}*top = NULL;
+
+//Initialize push function
+
+void push(int data)
+{
+	struct node *temp;
+	temp =(struct node*) malloc (sizeof(struct node));
+	//check if stack is full
+	if(temp == NULL)
+	{
+		printf("Stack overflow\n");
+		exit(1);
+	}
+	//to get the data
+	temp->data = data;
+	temp->link = NULL;
+	temp->link = top;
+	top = temp;
+	//return top;
+}
+void seek()
+{
+	printf("The top most data in stack is %d\n", top->data);
+}
+	
+void print()
+{
+	struct node *temp;
+	temp = malloc(sizeof(struct node));
+	temp = top;
+	while(temp != NULL)
+	{
+		printf("%d\n", temp->data);
+		temp = temp->link;
+	}
+}
+int main()
+{
+	int num, choice;
+	while(true)
+	{
+		printf("Welkcome to push stack\n");
+		printf(".......................\n");
+		printf("\nMake a choice\n");
+		printf("1) Push\n");
+		printf("2) print all\n");
+		printf("3) print top most data\n");
+		printf("4) Quit\n");
+		scanf("%d", &choice);
+
+
+		//use switch case to choose what to do
+
+		switch(choice)
+		{
+			case 1:
+				printf("Enter the number to push\n");
+				scanf("%d", &num);
+				push(num);
+				break;
+			case 2:
+				printf("The data in the stack\n");
+				print();
+				break;
+			case 3:
+				seek();
+				break;
+			case 4:
+				printf("Shutting down\n");
+				exit(1);
+				break;
+			default:
+				printf("Enter a valid number\n");
+		}
+	}
+	return (0);
+}
